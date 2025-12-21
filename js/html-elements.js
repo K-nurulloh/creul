2d1dae898b92d5aec5ac46061fe1b0f55d70a90d
Nurulloh <nurullohkomilov18@gmail.com>
2025-12-22 00:35:16 +0500

    math

--- js/html-elements.js
+++ js/html-elements.js
@@ -1,4 +1,6 @@
 const elMathProblem = document.getElementById("mathProblem")
 const elBox = document.getElementById("box")
+const elWin = document.getElementById("win")
+const elLoser = document.getElementById("loser")
 
-export { elMathProblem , elBox }
+export { elMathProblem , elWin , elBox , elLoser }
